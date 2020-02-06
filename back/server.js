c88c121e3606648ae121ae68c26bced72ff74b20
h662 <godh662@gmail.com>
2020-02-07 00:05:59 +0900

    xhr get

--- back/server.js
+++ back/server.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 8001
 
+app.use(cors())
+
 app.get('/', (req, res) => res.send('Hello Node.js'))
+
+// cors issue
+// https://developer.mozilla.org/ko/docs/Web/HTTP/CORS
+app.get('/hello', (req, res) => {    
+    res.send('Hello')
+    console.log('connected');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
